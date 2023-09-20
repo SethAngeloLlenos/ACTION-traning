b77a5c5861a9a1c4d8bbde2f03dbcf93f7573dfb
SethAngeloLlenos <llenosseth.aws@gmail.com>
2023-09-20 14:51:42 +0800

    this branch's something new

--- src/gitLectureCtachUp/SampleClass.java
+++ src/gitLectureCtachUp/SampleClass.java
@@ -5,6 +5,7 @@ public class SampleClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 /*leaving a comment here through giHub*/
+		/*something new in this new branch*/
 	}
 
 }
